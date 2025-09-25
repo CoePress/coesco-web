68f29572581296675d5e78db3cb3aff9f45bb5a2
Alex Kyrtsos <atk@cpec.com>
2025-09-25 14:21:47 -0400

    begone contact-type

--- packages/types/src/contact-type.ts
+++
@@ -1,9 +0,0 @@
-// Auto-generated from Prisma schema
-export enum ContactType {
-  Accounting = "A",
-  Engineering = "E",
-  Inactive = "I",
-  Left_Company = "L",
-  Parts_Service = "P",
-  Sales = "S",
-}
