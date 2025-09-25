05a1b02c2c048bc237342f3d1892c78f31af5538
Jacob <jar@cpec.com>
2025-09-25 10:20:16 -0400

    added employee initals to auth context

--- apps/server/src/utils/context.ts
+++ apps/server/src/utils/context.ts
@@ -7,6 +7,7 @@ export interface EmployeeContext {
   email: string;
   title: string;
   number: string;
+  initials: string
 }
 
 export const contextStorage = new AsyncLocalStorage<EmployeeContext>();
