b983084723869c8a296b1085a569c4330aa1b462
Jacob <jar@cpec.com>
2025-09-25 15:38:51 -0400

    removed service worker

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -25,11 +25,11 @@ createRoot(document.getElementById("root")!).render(
   </BrowserRouter>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => console.log("Service Worker registered:", reg))
-      .catch((err) => console.error("Service Worker registration failed:", err));
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/sw.js")
+//       .then((reg) => console.log("Service Worker registered:", reg))
+//       .catch((err) => console.error("Service Worker registration failed:", err));
+//   });
+// }
