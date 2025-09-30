e769a7f57c4516067ba90f93003eb59e0874dcc7
Jacob <jar@cpec.com>
2025-09-30 15:09:06 -0400

    report emails

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -66,8 +66,8 @@ app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // Raw body parsing for file uploads
 app.use("/api/files/upload", express.raw({ type: "*/*", limit: "100mb" }));
 app.set("trust proxy", false);
