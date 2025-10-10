710babde5d8abf22c03693c06013990946de6783
Jacob <jar@cpec.com>
2025-10-10 09:41:41 -0400

    a lot

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -1,35 +1,73 @@
-import { AdminController } from "./admin.controller";
-import { AuditLogController } from "./audit-log.controller";
-import { AuthController } from "./auth.controller";
-import { ChatController } from "./chat.controller";
-import { ConfigurationController } from "./configuration.controller";
-import { CRMController } from "./crm.controller";
-import { ExternalInvitationController } from "./external-invitation.controller";
-import { FormController } from "./form.controller";
-import { LegacyController } from "./legacy.controller";
-import { LockController } from "./lock.controller";
-import { PerformanceController } from "./performance.controller";
-import { PostalCodeController } from "./postal-code.controller";
-import { ProductionController } from "./production.controller";
-import { QuoteController } from "./quote.controller";
-import { SettingsController } from "./settings.controller";
-import { SystemController } from "./system.controller";
-import { TagController } from "./tag.controller";
+// import { AdminController } from "./admin.controller";
+// import { AuditLogController } from "./audit-log.controller";
+// import { AuthController } from "./auth.controller";
+// import { ChatController } from "./chat.controller";
+// import { ConfigurationController } from "./configuration.controller";
+// import { CRMController } from "./crm.controller";
+// import { ExternalInvitationController } from "./external-invitation.controller";
+// import { FormController } from "./form.controller";
+// import { LegacyController } from "./legacy.controller";
+// import { LockController } from "./lock.controller";
+// import { PerformanceController } from "./performance.controller";
+// import { PostalCodeController } from "./postal-code.controller";
+// import { ProductionController } from "./production.controller";
+// import { QuoteController } from "./quote.controller";
+// import { SettingsController } from "./settings.controller";
+// import { SystemController } from "./system.controller";
+// import { TagController } from "./tag.controller";
 
-export const adminController = new AdminController();
-export const auditLogController = new AuditLogController();
-export const authController = new AuthController();
-export const chatController = new ChatController();
-export const configurationController = new ConfigurationController();
-export const crmController = new CRMController();
-export const externalInvitationController = new ExternalInvitationController();
-export const formController = new FormController();
-export const legacyController = new LegacyController();
-export const lockController = new LockController();
-export const performanceController = new PerformanceController();
-export const postalCodeController = new PostalCodeController();
-export const productionController = new ProductionController();
-export const quoteController = new QuoteController();
-export const settingsController = new SettingsController();
-export const systemController = new SystemController();
-export const tagController = new TagController();
+import { AuditController } from "./admin/audit.controller";
+import { BugReportingController } from "./admin/bug-reporting.cotroller";
+import { EmployeeController } from "./admin/employee.controller";
+import { PermissionController } from "./admin/permission.controller";
+import { AuthController } from "./auth.controller";
+import { LegacyController } from "./core/legacy.controller";
+import { LockController } from "./core/lock.controller";
+import { CustomerController } from "./sales/customer.controller";
+import { JourneyController } from "./sales/journey.controller";
+import { QuoteController } from "./sales/quote.controller";
+import { ResourceMonitoringController } from "./production/resource-monitoring.controller";
+import { ResourceController } from "./production/resource.controller";
+import { SettingsController } from "./core/settings.controller";
+import { PerformanceSheetController } from "./artifact/performance-sheet.controller";
+import { ServiceReportController } from "./artifact/service-report.controller";
+
+// export const adminController = new AdminController();
+// export const chatController = new ChatController();
+// export const configurationController = new ConfigurationController();
+// export const crmController = new CRMController();
+// export const externalInvitationController = new ExternalInvitationController();
+// export const formController = new FormController();
+// export const legacyController = new LegacyController();
+// export const performanceController = new PerformanceController();
+// export const postalCodeController = new PostalCodeController();
+// export const productionController = new ProductionController();
+// export const quoteController = new QuoteController();
+// export const settingsController = new SettingsController();
+// export const systemController = new SystemController();
+// export const tagController = new TagController();
+
+// Admin
+export const auditController = new AuditController()
+export const bugReportingController = new BugReportingController()
+export const employeeController = new EmployeeController()
+export const permissionController = new PermissionController()
+
+// Artifact
+export const performanceSheetController = new PerformanceSheetController()
+export const serviceReportController = new ServiceReportController()
+
+// Core
+export const authController = new AuthController()
+export const legacyController = new LegacyController()
+export const lockController = new LockController()
+export const settingsController = new SettingsController()
+
+// Production
+export const resourceMonitoringController = new ResourceMonitoringController()
+export const resourceController = new ResourceController()
+
+// Sales
+export const customerController = new CustomerController()
+export const journeyController = new JourneyController()
+export const quoteController = new QuoteController()
--- apps/server/src/services/index.ts
+++ apps/server/src/services/index.ts
@@ -1,63 +1,110 @@
-import { io } from "@/app";
-import { __prod__ } from "@/config/env";
+// import { io } from "@/app";
+// import { __prod__ } from "@/config/env";
 
-import { AgentService } from "./business/agent.service";
-import { AuditLogBusinessService } from "./business/audit-log.service";
-import { DeviceService } from "./business/device.service";
-import { EmployeeSyncService } from "./business/employee-sync.service";
-import { FormManagerService } from "./business/form-manager.service";
-import { LegacyService } from "./business/legacy.service";
-import { LocationService } from "./business/location.service";
-import { MachineMonitorService } from "./business/machining.service";
-import { MicrosoftService } from "./business/microsoft.service";
-import { QuotingService } from "./business/quoting.service";
-import { SettingsService } from "./business/settings.service";
-import { AuthService } from "./core/auth.service";
+import { AuditServicce } from "./admin/audit.service";
+import { BugReportingService } from "./admin/bug-reporting.service";
+import { EmployeeService } from "./admin/employee.service";
+import { ConfigurationService } from "./catalog/configuration.service";
+import { OptionService } from "./catalog/option.service";
+import { ProductService } from "./catalog/product.service";
 import { CacheService } from "./core/cache.service";
 import { EmailService } from "./core/email.service";
-import { ExternalInvitationService } from "./core/external-invitation.service";
-import { FileStorageService } from "./core/file-storage.service";
-import { JiraService } from "./core/jira.service";
 import { LockingService } from "./core/locking.service";
-import { MCPService } from "./core/mcp.service";
 import { PermissionService } from "./core/permission.service";
 import { SocketService } from "./core/socket.service";
+import { CustomerService } from "./sales/customer.service";
+import { JourneyService } from "./sales/journey.service";
+import { QuoteService } from "./sales/quote.service";
+import { ResourceMonitoringSerivce } from "./production/resource-monitoring.service";
+import { ResourceService } from "./production/resource.service";
 
-// Business
-export const agentService = new AgentService();
-export const auditLogBusinessService = new AuditLogBusinessService();
-export const deviceService = new DeviceService();
-export const employeeSyncService = new EmployeeSyncService();
-export const formManagerService = new FormManagerService();
-export const legacyService = new LegacyService();
-export const locationService = new LocationService();
-export const machiningService = new MachineMonitorService();
-export const microsoftService = new MicrosoftService();
-export const quotingService = new QuotingService();
-export const settingsService = new SettingsService();
+// import { AgentService } from "./business/agent.service";
+// import { AuditLogBusinessService } from "./business/audit-log.service";
+// import { DeviceService } from "./business/device.service";
+// import { EmployeeSyncService } from "./business/employee-sync.service";
+// import { FormManagerService } from "./business/form-manager.service";
+// import { LegacyService } from "./business/legacy.service";
+// import { LocationService } from "./business/location.service";
+// import { MachineMonitorService } from "./business/machining.service";
+// import { MicrosoftService } from "./business/microsoft.service";
+// import { QuotingService } from "./business/quoting.service";
+// import { SettingsService } from "./business/settings.service";
+// import { AuthService } from "./core/auth.service";
+// import { CacheService } from "./core/cache.service";
+// import { EmailService } from "./core/email.service";
+// import { ExternalInvitationService } from "./core/external-invitation.service";
+// import { FileStorageService } from "./core/file-storage.service";
+// import { JiraService } from "./core/jira.service";
+// import { LockingService } from "./core/locking.service";
+// import { MCPService } from "./core/mcp.service";
+// import { PermissionService } from "./core/permission.service";
+// import { SocketService } from "./core/socket.service";
+
+// // Business
+// export const agentService = new AgentService();
+// export const auditLogBusinessService = new AuditLogBusinessService();
+// export const deviceService = new DeviceService();
+// export const employeeSyncService = new EmployeeSyncService();
+// export const formManagerService = new FormManagerService();
+// export const legacyService = new LegacyService();
+// export const locationService = new LocationService();
+// export const machiningService = new MachineMonitorService();
+// export const microsoftService = new MicrosoftService();
+// export const quotingService = new QuotingService();
+// export const settingsService = new SettingsService();
+
+// // Core
+// export const authService = new AuthService();
+// export const cacheService = new CacheService();
+// export const emailService = new EmailService();
+// export const externalInvitationService = new ExternalInvitationService();
+// export const fileStorageService = new FileStorageService();
+// export const jiraService = new JiraService();
+// export const lockingService = new LockingService();
+// export const mcpService = new MCPService();
+// export const permissionService = new PermissionService();
+// export const socketService = new SocketService();
+
+// export async function initializeServices() {
+//   await deviceService.initialize();
+//   await legacyService.initialize();
+//   await mcpService.initialize();
+//   await socketService.initialize(io);
+//   await authService.initializeDefaultUser();
+
+//   const collectMachineData = false;
+
+//   if (__prod__ || collectMachineData) {
+//     await machiningService.initialize();
+//   }
+// }
+
+
+// Admin
+export const auditService = new AuditServicce()
+export const bugReportingServie = new BugReportingService()
+export const employeeService = new EmployeeService()
+export const permissionService = new PermissionService()
+
+// Catalog
+export const configurationService = new ConfigurationService()
+export const optionService = new OptionService()
+export const productService = new ProductService()
+
 
 // Core
-export const authService = new AuthService();
-export const cacheService = new CacheService();
-export const emailService = new EmailService();
-export const externalInvitationService = new ExternalInvitationService();
-export const fileStorageService = new FileStorageService();
-export const jiraService = new JiraService();
-export const lockingService = new LockingService();
-export const mcpService = new MCPService();
-export const permissionService = new PermissionService();
-export const socketService = new SocketService();
+export const cacheService = new CacheService()
+export const emailServicce = new EmailService()
+export const lockingService = new LockingService()
+export const socketService = new SocketService()
 
-export async function initializeServices() {
-  await deviceService.initialize();
-  await legacyService.initialize();
-  await mcpService.initialize();
-  await socketService.initialize(io);
-  await authService.initializeDefaultUser();
+// Production
+export const resourceMonitoringService = new ResourceMonitoringSerivce()
+export const resourceService = new ResourceService()
 
-  const collectMachineData = false;
+// Sales
+export const customerService = new CustomerService()
+export const journeyService = new JourneyService()
+export const quoteService = new QuoteService()
 
-  if (__prod__ || collectMachineData) {
-    await machiningService.initialize();
-  }
-}
+// Time Clock
